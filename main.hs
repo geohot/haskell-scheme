2324b04f47b0f2d289a66140fe5dd73c9121466b
George Hotz <geohot@gmail.com>
2020-08-02 18:15:38 -0400

    main hs

---
+++ main.hs
@@ -0,0 +1,70 @@
+module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+import Numeric
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | Float Float
+             | String String
+             | Bool Bool
+             deriving Show
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of
+                "#t" -> Bool True
+                "#f" -> Bool False
+                _    -> Atom atom
+
+
+parseNumber :: Parser LispVal
+parseNumber = do
+  x <- many1 digit
+  return $ Number $ read x
+-- parseNumber = liftM (Number . read) $ many1 digit
+
+parseFloat :: Parser LispVal
+parseFloat = do
+  x <- many1 digit 
+  char '.'
+  y <- many1 digit
+  let atom = (x ++ "." ++ y)
+  return $ Float $ read atom
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseFloat
+        <|> parseNumber
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value" ++ show val
+
+main :: IO ()
+main = do
+  (expr:_) <- getArgs
+  putStrLn (readExpr expr)
+
