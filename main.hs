600ccd3009e862a788c4156e7ae4079385850ee1
George Hotz <geohot@gmail.com>
2020-08-02 18:36:43 -0400

    parse quoted

--- main.hs
+++ main.hs
@@ -49,8 +49,13 @@ parseFloat = do
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
         <|> parseString
-        <|> parseFloat
+        <|> try parseFloat
         <|> parseNumber
+        <|> parseQuoted
+        <|> do char '('
+               x <- try parseList <|> parseDottedList
+               char ')'
+               return x
 
 parseList :: Parser LispVal
 parseList = liftM List $ sepBy parseExpr spaces
@@ -60,6 +65,12 @@ parseDottedList = do
   head <- endBy parseExpr spaces
   tail <- char '.' >> spaces >> parseExpr
   return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
 
 spaces :: Parser ()
 spaces = skipMany1 space
